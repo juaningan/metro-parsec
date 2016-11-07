438216f7a18ee98e6bbf43dbbb6edabe4e7994c2
Juanjo Presa <Juanjo Presa>
2016-11-07 12:50:25 +0100

    Add iprb to dhcp

--- tce-estacion/packer/scripts/tce-rootfs.sh
+++ tce-estacion/packer/scripts/tce-rootfs.sh
@@ -5,4 +5,4 @@ set -e
 
 # Run mvi
 cd /packages/localsrc/Tribblix/mvi/
-bash -e -x ./mvi-noiso.sh -s 32bit dhcp debug realport motif tce xorg wget tce-drivers
+bash -e -x ./mvi-noiso.sh -s 32bit dhcp dhcp-iprb debug realport motif tce xorg wget tce-drivers
