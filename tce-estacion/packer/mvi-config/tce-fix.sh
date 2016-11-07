5708233a6628bab5427f0922451f72791dbccc48
Juanjo Presa <Juanjo Presa>
2016-11-07 11:53:12 +0100

    Remove debug lines

--- tce-estacion/packer/mvi-config/tce-fix.sh
+++ tce-estacion/packer/mvi-config/tce-fix.sh
@@ -8,9 +8,6 @@ TSIZE=`echo $MRSIZE | sed s:M::`
 TSIZE=$(($TSIZE+90))
 MRSIZE=${TSIZE}M
 
-echo $DESTDIR
-echo $PWD
-
 wget -q "https://github.com/juaningan/tce-estacion-config/archive/master.tar.gz" -O - | /tmp/usr/gnu/bin/tar -C ${DESTDIR} -xzf - tce-estacion-config-master/common --strip-components=2
 
 sync
