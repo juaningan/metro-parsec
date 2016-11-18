b4de7ab5c889ed2a04d82875440e76e55c67514b
Juanjo Presa <Juanjo Presa>
2016-11-18 14:28:38 +0100

    Refactor to-disk

--- tce-estacion/packer/scripts/tce-to-disk.sh
+++ tce-estacion/packer/scripts/tce-to-disk.sh
@@ -6,4 +6,4 @@ set -e
 # Run mvi
 cd /packages/localsrc/Tribblix/mvi/
 #bash -e -x ./mvi-mkfs.sh -s 32bit debug dhcp wget tce-to-disk realport fvwm tce xorg ssh
-bash -e -x ./mvi-mkfs.sh -s 32bit debug dhcp wget tce-to-disk
+bash -e -x ./mvi-mkfs.sh -s 32bit debug wget ife tce-net tce-to-disk
