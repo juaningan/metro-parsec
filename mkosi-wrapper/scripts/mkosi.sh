eebb176eefafb67596d84ffddfcfe947d9ddbcf2
Juanjo Presa <juanjop@gmail.com>
2017-12-05 14:44:53 +0100

    Change mkosi.json

--- mkosi-wrapper/scripts/mkosi.sh
+++ mkosi-wrapper/scripts/mkosi.sh
@@ -4,4 +4,5 @@ set -x
 
 sed -i "s/DEBIAN_FRONTEND/http_proxy': 'http:\/\/16.0.96.20:3128', 'DEBIAN_FRONTEND/g" /usr/bin/mkosi
 mkdir -p /tmp/output
-mkosi --output-dir=/tmp/output/
+mkdir -p /tmp/cache
+mkosi --xz --cache=/tmp/cache --output-dir=/tmp/output/
