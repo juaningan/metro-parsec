a01c1c860adc7f0f0eed481370a99f8fd5650b4a
Juanjo Presa <juanjop@gmail.com>
2017-12-12 12:34:37 +0100

    Add workdir logic

--- packer/mkosi-wrapper/scripts/mkosi-wrapper.sh
+++ packer/mkosi-wrapper/scripts/mkosi-wrapper.sh
@@ -5,4 +5,4 @@ set -x
 sed -i "s/DEBIAN_FRONTEND/http_proxy': 'http:\/\/16.0.96.20:3128', 'DEBIAN_FRONTEND/g" /usr/bin/mkosi
 mkdir -p /tmp/output
 mkdir -p /tmp/cache
-mkosi --xz --cache=/tmp/cache --output-dir=/tmp/output/
+mkosi -C /tmp/workdir --xz --cache=/tmp/cache --output-dir=/tmp/output/
