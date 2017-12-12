8fa0b32b2dd803b2fbc1a0189387e4eda2f572bd
Juanjo Presa <juanjop@gmail.com>
2017-12-12 14:36:22 +0100

    add proxy to wrapper

--- packer/mkosi-wrapper/scripts/mkosi-wrapper.sh
+++ packer/mkosi-wrapper/scripts/mkosi-wrapper.sh
@@ -5,4 +5,8 @@ set -x
 sed -i "s/DEBIAN_FRONTEND/http_proxy': 'http:\/\/16.0.96.20:3128', 'DEBIAN_FRONTEND/g" /usr/bin/mkosi
 mkdir -p /tmp/output
 mkdir -p /tmp/cache
+
+export http_proxy="http://16.0.96.20:3128"
+export https_proxy="http://16.0.96.20:3128"
+
 mkosi -C /tmp/workdir --xz --cache=/tmp/cache --output-dir=/tmp/output/
